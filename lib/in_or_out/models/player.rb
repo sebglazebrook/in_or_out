f925ecd0855492e172670680ed74ad22bb692f67
Sebastian Glazebrook <sebastian.glazebrook@rea-group.com>
2013-04-02 14:56:22 +1100

    Added optional arguments to player initializer

--- lib/in_or_out/models/player.rb
+++ lib/in_or_out/models/player.rb
@@ -1,10 +1,11 @@
 module InOrOut
   class Player
 
-    attr_reader :status, :position
+    attr_reader :number, :position
 
-    def initialize(player_name, team_name, status: nil, position: nil, number: nil)
+    def initialize(player_name, team_name,  **options)
       @name, @team = player_name, team_name
+      @number, @position = options[:number], options[:position]
     end
 
   end
--- spec/in_and_out/models/player_spec.rb
+++ spec/in_and_out/models/player_spec.rb
@@ -7,34 +7,19 @@ describe InOrOut::Player do
   let(:team_name) { 'Essendon' }
   let(:analyser_response) { OpenStruct.new(position: 'Centre', status: 'In') }
 
-  before do
-    InOrOut::Analyser.any_instance.should_receive(:analyse).and_return(analyser_response)
-  end
-
   describe 'initialize' do
 
     let(:subject) { InOrOut::Player }
 
-    it 'takes a player name and a team name' do
+    it 'requires a player and team name' do
       subject.new(player_name, team_name).class.should == InOrOut::Player
     end
 
-    it 'analyses the player and sets their status and position' do
-      player = subject.new(player_name, team_name)
-      player.status.should == analyser_response.status
-      player.position.should == analyser_response.position
-    end
-
-  end
-
-  describe 'attributes' do
-
-    let(:subject) { InOrOut::Player.new(player_name, team_name)}
-
-    %i(status position).each do |attribute|
-      it "has a #{attribute}" do
-        subject.respond_to?(attribute).should == true
+    %w(number position).each do |attr|
+      it "accepts '#{attr}' as an optional attribute" do
+        subject.new(player_name, team_name, attr.to_sym => 'attribute value').send(attr.to_sym).should == 'attribute value'
       end
+
     end
 
   end
