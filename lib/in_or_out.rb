b8b82233a967b3404c0726d435dd36a8bbb8aa2e
Sebastian Glazebrook <sebastian.glazebrook@rea-group.com>
2013-04-09 13:38:03 +1000

    Deleted no longer used Analyser class

--- lib/in_or_out.rb
+++ lib/in_or_out.rb
@@ -1,9 +1,10 @@
 require 'in_or_out/version'
-require 'in_or_out/models/analyser'
 require 'in_or_out/models/player'
 require 'in_or_out/models/scraper'
 require 'in_or_out/models/team'
 require 'in_or_out/models/match'
+require 'in_or_out/models/player_extractor'
+require 'in_or_out/models/shortcode_converter'
 
 require 'yaml'
 require 'active_support/core_ext/hash/indifferent_access'
